0e08ed3904942b0d4cb247d99969f83e14b5f5fb
taylorkeltgen <taylorkeltgen@gmail.com>
2021-09-26 19:00:15 -0500

    added js code

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,18 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+
+
+// this creates/declares a function named "fight"
+function fight() {
+    window.alert("The fight has begun!");
+  }
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10 + 10);
+// what is this?
+console.log("Our robot's name is " + playerName);
+
+// calling the function "fight"
+fight();
