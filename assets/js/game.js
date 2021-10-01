392a5069a18ddc449630a035cbbc33132264a0ab
taylorkeltgen <taylorkeltgen@gmail.com>
2021-09-30 20:43:19 -0500

    built Store feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,4 @@
+debugger;
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
@@ -17,8 +18,8 @@ console.log(enemyNames[3]);
 //    * Fight all enemy-robots
 //    * Defeat each enemy-robot
 // "LOSE" - Player robot's health is zero or less
-
-// Defining the "Fight" Function
+debugger;
+/// FIGHT Function ///
 var fight = function(enemyName) {
     while(playerHealth > 0 && enemyHealth > 0) {
         // Ask the player for an option.
@@ -36,7 +37,6 @@ var fight = function(enemyName) {
                 break;
             }
         }
-
         // remove enemy's health by subtracting the amount set in the playerAttack variable
         enemyHealth = enemyHealth - playerAttack;
         console.log(
@@ -70,6 +70,7 @@ var fight = function(enemyName) {
     }
 };
 
+/// START GAME FUNCTION ///
 var startGame = function() {   
      // reset player stats
     playerHealth = 100;
@@ -89,6 +90,17 @@ var startGame = function() {
 
             // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
             fight(pickedEnemyName);
+
+            // if player is still alive AND we're not at the last enemy in the array
+            if (playerHealth > 0 && i < enemyNames.length - 1) {
+                // ask if player wants to use the store before next round
+                var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+
+                // if yes, take them to the store() function
+                if (storeConfirm) {
+                    shop();
+                }
+            }
         } else {
                 window.alert("You have lost your robot in battle! Game Over!");
                 break;
@@ -99,7 +111,7 @@ var startGame = function() {
     endGame();
 };
 
-// function to end the entire game
+/// END GAME FUNCTION - function to end the entire game
 var endGame = function() {
     // if player is still alive, player wins!
     if (playerHealth > 0) {
@@ -119,6 +131,55 @@ var endGame = function() {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
     }
   }
+
+/// SHOP FEATURE FUNCTION ///
+var shop = function() {
+    // ask player what they'd like to do
+    var shopOptionPrompt = window.prompt(
+        "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+
+    // use switch to carry out action
+    switch (shopOptionPrompt) {
+        case "REFILL":
+        case "refill":
+            if (playerMoney >= 7) {
+                window.alert("Refilling player's health by 20 for 7 dollars.");
+                // increase health and decrease money
+                playerHealth = playerHealth + 20;
+                playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+
+        case "UPGRADE":
+        case "upgrade":
+            if (playerMoney >= 7) {
+                window.alert("Upgrading player's attack by 6 for 7 dollars.");
+                // increase attack and decrease money
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+            }
+            else {
+                window.alert("You don't have enough money!");
+            }
+            break;
+
+        case "LEAVE":
+        case "leave":
+        window.alert("Leaving the store.");
+        // do nothing, so function will end
+        break;
+
+        default:
+        window.alert("You did not pick a valid option. Try again.");
+        // call shop() again to force player to pick a valid option
+        shop();
+        break;
+    }
+};
 
  // Start the game when the page loads
  startGame();
